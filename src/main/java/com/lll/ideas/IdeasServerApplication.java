33e1a0e465aff8f8ed5ec77d74613e6cdd260f0e
QiuYiliang <1162922438@qq.com>
2021-03-14 00:25:48 +0800

    初始化项目

---
+++ src/main/java/com/lll/ideas/IdeasServerApplication.java
@@ -0,0 +1,13 @@
+package com.lll.ideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeasServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeasServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lll/ideas/IdeasServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lll.ideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeasServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
