00d4a5422d12e4e9843c3b1001b32de46d2b8894
cddorz <2878153350@qq.com>
2021-03-14 19:57:44 +0800

    user

--- src/main/java/com/lll/ideas/pojo/User.java
+++ src/main/java/com/lll/ideas/pojo/User.java
@@ -15,13 +15,38 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    /**
+     * 用户id
+     */
     private Integer userId;
+
+    /**
+     * 用户名
+     */
     private String username;
+    /**
+     * 用户密码
+     */
     private String password;
+    /**
+     * 用户性别
+     */
     private Byte gender;
+    /**
+     * 用户电话
+     */
     private String phone;
+    /**
+     * 个人简介
+     */
     private String info;
+    /**
+     * 用户头像
+     */
     private String avatar;
+    /**
+     * 创建时间
+     */
     private Date createTime;
 
 
